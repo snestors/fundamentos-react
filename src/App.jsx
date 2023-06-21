e727544ed30b894803119b7cfc7740c702f8b08c
Nestor <snestors@gmail.com>
2023-06-21 16:50:30 -0500

    comint 21062023

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         <h1 className={atrib}>Hola React</h1>
         <div className="img-center">
         <img className="imagen-circular" src="https://picsum.photos/200/300" alt="" />
-        <h2>Test Git</h2>
+        
         </div>
 
         </>
