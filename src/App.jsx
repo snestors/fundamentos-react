de2a3ddf4fe03f70c97e3e21cadb5f13bf61772b
Nestor <snestors@gmail.com>
2023-06-21 16:45:03 -0500

    curso 21062023

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+
+    const atrib = "text-center"
+
+    return (
+        <>
+        <h1 className={atrib}>Hola React</h1>
+        <div className="img-center">
+        <img className="imagen-circular" src="https://picsum.photos/200/300" alt="" />
+        </div>
+
+        </>
+    )
+
+}
+
+export default App;
